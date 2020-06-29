af8cd301627e297f7ad89e50ce2582cedc0c4a01
JG-Lembo <joao.gabriel200022@gmail.com>
2020-06-28 22:19:49 -0300

    Logout arrumado e novo método de escolher tags

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy'
 
   get 'cadastro', to: 'segues#new'
 
